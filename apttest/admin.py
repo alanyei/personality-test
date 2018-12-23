437287b042d139ae17f2cbff485914824fc2f8b3
alanyei <alanyeh@outlook.com>
2018-12-23 11:38:54 +0800

    Add admin import/export module

--- apttest/admin.py
+++ apttest/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
+# Import import_export model for admin
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 from .models import Result
 
-admin.site.register(Result);
+@admin.register(Result)
+class PersonAdmin(ImportExportModelAdmin):
+     list_display = ('id', 'name', 'sex', 'email', 'age', 'answer', 'person_type', 'work_exp',  'submitted_date')
+
+#admin.site.register(Result);
